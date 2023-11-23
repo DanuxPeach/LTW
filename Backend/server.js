2d17fb39476d432a8b0db5a486438d71d34f3c93
DanuxPeach <21520682@gm.uit.edu.vn>
2023-11-23 08:58:22 +0700

    Upload Backend folder

---
+++ Backend/server.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const app = express();
+const port = 5000;
+const cors = require('cors')
+const mysql = require('mysql')  
+
+app.use(express.json());
+app.use(cors())
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
+// Kết nối đến MySQL
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root', // Thay đổi tên người dùng và mật khẩu nếu cần
+    password: '',
+    database: 'users', // Tên của cơ sở dữ liệu
+  });
+
+connection.connect((err) => {
+if (err) {
+    console.error('Error connecting to MySQL:', err);
+    return;
+}
+console.log('Connected to MySQL!');
+});
+
+    // Endpoint để lưu thông tin người dùng vào cơ sở dữ liệu SQL Server
+app.post('/register', (req, res) => {
+    const sentName = req.body.Name
+    const sentEmail = req.body.Email
+    const sentPassword = req.body.Password
+
+    const SQL = 'INSERT INTO user (Name, Email, Password) VALUES (?,?,?)'
+    const Values = [sentName, sentEmail, sentPassword]
+
+    connection.query(SQL, Values, (err, results)=>{
+        if(err){
+            res.send(err)
+        }
+        else{
+            console.log('User inserted success!')
+            res.send({message: 'User added!'})
+        }
+    })
+})
+
+// Endpoint để xử lý yêu cầu đăng nhập từ frontend
+app.post('/login', (req, res) => {
+    const sentLoginEmail = req.body.LoginEmail
+    const sentLoginPassword = req.body.LoginPassword
+
+    const SQL = 'SELECT * FROM user WHERE Email = ? && Password = ?'
+    const Values = [ sentLoginEmail, sentLoginPassword]
+    // Thực hiện xác thực thông tin đăng nhập ở đây, ví dụ:
+
+    connection.query(SQL, Values, (err, results)=>{
+        if(err){
+            res.send({error: err})
+        }
+        if(results.length > 0){
+            res.send(results)
+        }
+        else{
+            res.send({message:'Credentials Don\'t Match!'})
+        }
+    })
+})
