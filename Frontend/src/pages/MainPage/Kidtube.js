ba107b04bd2568bf826ae3b380328bceda885daf
DanuxPeach <21520682@gm.uit.edu.vn>
2023-11-28 00:45:06 +0700

    update something

--- Frontend/src/pages/MainPage/Kidtube.js
+++ Frontend/src/pages/MainPage/Kidtube.js
@@ -1,58 +1,32 @@
 import { useCallback } from "react";
 import { useNavigate } from "react-router-dom";
 import "./Kidtube.css";
+import Header from "../../components/header/Header";
 
 const Kidtube = () => {
   const navigate = useNavigate();
 
-  const onVid2ContainerClick = useCallback(() => {
+  const onVidContainerClick = useCallback(() => {
     navigate("/video");
   }, [navigate]);
 
-  const onVid1ContainerClick = useCallback(() => {
-    navigate("/video");
-  }, [navigate]);
-
-  const onCate1Click = useCallback(() => {
+  const onCategoryClick = useCallback(() => {
     navigate("/list");
-  }, [navigate]);
-
-  const onCate2ContainerClick = useCallback(() => {
-    navigate("/list");
-  }, [navigate]);
-
-  const onCate3ContainerClick = useCallback(() => {
-    navigate("/list");
-  }, [navigate]);
-
-  const onCate4ContainerClick = useCallback(() => {
-    navigate("/list");
-  }, [navigate]);
-
-  const onCate5ContainerClick = useCallback(() => {
-    navigate("/list");
-  }, [navigate]);
-
-  const onSignUpClick = useCallback(() => {
-    navigate("/register");
-  }, [navigate]);
-
-  const onLoginClick = useCallback(() => {
-    navigate("/login");
   }, [navigate]);
 
   return (
     <div className="kidtube">
+      < Header />
       <div className="decor">
         <img className="image-9-icon" alt="" src="/image-9@2x.png" />
         <img className="image-10-icon" alt="" src="/image-10@2x.png" />
       </div>
       <div className="rcm">
-        <div className="vid2" onClick={onVid2ContainerClick}>
+        <div className="vid2" onClick={onVidContainerClick}>
           <div className="lexis-adventure-time">Lexi’s adventure time</div>
           <img className="image-8-icon" alt="" src="/image-8@2x.png" />
         </div>
-        <div className="vid1" onClick={onVid1ContainerClick}>
+        <div className="vid1" onClick={onVidContainerClick}>
           <div className="new-sweetie">New | Sweetie paradise for children</div>
           <img className="image-8-icon" alt="" src="/image-7@2x.png" />
         </div>
@@ -67,41 +41,27 @@ const Kidtube = () => {
         <div className="recommend-for-you">Recommend for you</div>
       </div>
       <div className="category">
-        <main className="cate1" onClick={onCate1Click}>
+        <main className="cate1" onClick={onCategoryClick}>
           <div className="animals">Animals</div>
           <img className="thumnail1-icon" alt="" src="/thumnail1@2x.png" />
         </main>
-        <div className="cate2" onClick={onCate2ContainerClick}>
+        <div className="cate2" onClick={onCategoryClick}>
           <div className="art">Art</div>
           <img className="thumnail1-icon" alt="" src="/thumnail2@2x.png" />
         </div>
-        <div className="cate3" onClick={onCate3ContainerClick}>
+        <div className="cate3" onClick={onCategoryClick}>
           <div className="creative">Creative</div>
           <img className="thumnail1-icon" alt="" src="/thumnail3@2x.png" />
         </div>
-        <div className="cate4" onClick={onCate4ContainerClick}>
+        <div className="cate4" onClick={onCategoryClick}>
           <div className="numbers">Numbers</div>
           <img className="thumnail1-icon" alt="" src="/thumnail4@2x.png" />
         </div>
-        <div className="cate5" onClick={onCate5ContainerClick}>
+        <div className="cate5" onClick={onCategoryClick}>
           <div className="cartoons">Cartoons</div>
           <img className="thumnail1-icon" alt="" src="/thumnail41@2x.png" />
         </div>
         <div className="top-categories">Top categories</div>
-      </div>
-      <img className="home-icon" alt="" src="/home.svg" />
-      <div className="header">
-        <div className="logo">
-          <div className="kidtube1">Kidtube</div>
-          <img className="logo-child" alt="" src="/group-244.svg" />
-        </div>
-        <input className="search" placeholder="Search" type="text" />
-        <button className="sign-up" onClick={onSignUpClick}>
-          <div className="button">Sign up</div>
-        </button>
-        <button className="login" onClick={onLoginClick}>
-          <div className="button1">Sign in</div>
-        </button>
       </div>
     </div>
   );
